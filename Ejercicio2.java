289fe41f92816c306f63c2b85484a049c96c0338
kuzafkto <kuzafkto@gmail.com>
2023-02-11 18:01:42 +0100

    Ejercicio 2

---
+++ Ejercicio2.java
@@ -0,0 +1,8 @@
+import EjerciciosClassVersion.Primos;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Primos primos=new Primos("primos.dat");
+        System.out.println(primos);
+    }
+}
